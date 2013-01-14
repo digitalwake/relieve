c7c45f7650e503cfb5d8b812b68f3d5d7a76c11b
Nicholas Romeo <nicholas@digitalwake.com>
2013-01-14 12:17:32 -0500

    Initials commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Relieve::Application.config.secret_token = '543ca0489ffb9b54473d01f20a5002f55054c913693d9b266e0e6d528f8e643f69efb8760679786b7afecb29ee653ae84642d0d7cdba49009aac305416836e50'
